9016c5fa52bc860decd51727ad542289399a64ed
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-10 05:32:46 -0300

    Initialize model classes.

---
+++ Category.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WorkingWithEFCore_Northwind
+{
+    public class Category
+    {
+        
+    }
+}
---
+++ Northwind.cs
@@ -0,0 +1,7 @@
+namespace WorkingWithEFCore_Northwind
+{
+    public class Northwind
+    {
+        
+    }
+}
