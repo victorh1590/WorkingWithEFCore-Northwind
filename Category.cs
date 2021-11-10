c42a960284b844e16533331e3abb4328dba59ed4
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-10 05:33:41 -0300

    Map table Category => class Category.

--- Category.cs
+++ Category.cs
@@ -5,6 +5,9 @@ namespace WorkingWithEFCore_Northwind
 {
     public class Category
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private ICollection<Product> Products { get; set; }= new HashSet<Product>();
     }
 }
