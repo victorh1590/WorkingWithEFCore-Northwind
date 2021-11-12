da123be24f5444268f65b2bc5865e7e6e3bc28d7
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-12 10:23:06 -0300

    Map Product => class Product.

--- Product.cs
+++ Product.cs
@@ -1,7 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WorkingWithEFCore_Northwind
 {
     public class Product
     {
+        public int Id { get; set; }
         
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+        
+        [Column("UnitPrice", TypeName = "money")]
+        public decimal? Cost { get; set; }
+        
+        [Column("UnitsInStock")]
+        public short? Stock { get; set; }
+        
+        public bool Discontinued { get; set; }
+        
+        public int CategoryId { get; set; }
+        
+        public virtual Category Category { get; set; }
     }
 }
