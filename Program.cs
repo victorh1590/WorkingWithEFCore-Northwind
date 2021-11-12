e40af9fa5439ba4ac2b1c3e2fbd2436a258ddffc
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-12 10:58:18 -0300

    Add query function for program execution.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using WorkingWithEFCore_Northwind;
 
-Console.WriteLine("Hello, World!");
+static void QueryingCategories()
+{
+    using (var db = new Northwind())
+    {
+        Console.WriteLine("Categories and how many products they have:");
+
+        IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+
+        foreach (Category c in cats)
+        {
+            Console.WriteLine($"{c.Name} has {c.Products.Count}.");
+        }
+    }
+}
+
+QueryingCategories();
+
