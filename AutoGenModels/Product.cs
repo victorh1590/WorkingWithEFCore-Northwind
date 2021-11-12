0d7a98894290a484f18ac70ec0bb1b4739f945a1
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-12 10:57:45 -0300

    Add scaffold class files for comparison.

---
+++ AutoGenModels/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkingWithEFCore_Northwind.AutoGen
+{
+    [Index(nameof(CategoryId), Name = "CategoriesProducts")]
+    [Index(nameof(CategoryId), Name = "CategoryID")]
+    [Index(nameof(ProductName), Name = "ProductName")]
+    [Index(nameof(SupplierId), Name = "SupplierID")]
+    [Index(nameof(SupplierId), Name = "SuppliersProducts")]
+    public partial class Product
+    {
+        [Key]
+        [Column("ProductID")]
+        public long ProductId { get; set; }
+        [Column(TypeName = "nvarchar (40)")]
+        public string ProductName { get; set; } = null!;
+        [Column("SupplierID", TypeName = "int")]
+        public long? SupplierId { get; set; }
+        [Column("CategoryID", TypeName = "int")]
+        public long? CategoryId { get; set; }
+        [Column(TypeName = "nvarchar (20)")]
+        public string? QuantityPerUnit { get; set; }
+        [Column(TypeName = "money")]
+        public byte[]? UnitPrice { get; set; }
+        [Column(TypeName = "smallint")]
+        public long? UnitsInStock { get; set; }
+        [Column(TypeName = "smallint")]
+        public long? UnitsOnOrder { get; set; }
+        [Column(TypeName = "smallint")]
+        public long? ReorderLevel { get; set; }
+        [Column(TypeName = "bit")]
+        public byte[] Discontinued { get; set; } = null!;
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty("Products")]
+        public virtual Category? Category { get; set; }
+    }
+}
