3e1fa14148ff70235253400f1fb6426b694e631d
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-12 10:23:55 -0300

    Build DbContext class Northwind. Add rules to modelbuilder (override). This is an option to attributes on table classes.

--- Northwind.cs
+++ Northwind.cs
@@ -1,7 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WorkingWithEFCore_Northwind
 {
-    public class Northwind
+    public class Northwind : DbContext
     {
-        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine
+                (System.Environment.CurrentDirectory, "Northwind.db");
+
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .Property(category => category.Name)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
     }
 }
